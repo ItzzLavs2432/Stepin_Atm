c7fbb240c8c3f30437ea98a05f5d6dc350607893
ItzzLavs2432 <89655608+ItzzLavs2432@users.noreply.github.com>
2021-09-08 18:19:14 +0530

    Update main.c

--- 3_Implementation/main.c
+++ 3_Implementation/main.c
@@ -1,201 +1,108 @@
-#include "header.h"
-
-int main(void) {
-    int choice1, choice2;
-    int pinValid = 0, balance = 0;
-
-    node *head = NULL;
-
-    while (1) {
-        printf("\n\nATM System\n======================\n");
-        printf("1. Generate PIN\n2. Use ATM\n3. Exit\n");
-        printf("\nYour choice: ");
-        scanf("%d", &choice1);
-
-        switch (choice1) {
-            case 1: pinGeneration();
-                    exit(EXIT_SUCCESS);
-
-            case 2: pinValid = checkPin();
-
-                    if (pinValid) {
-                        printf("\nValid PIN\n");
-                    } else {
-                        printf("\nInvalid PIN. Please generate a PIN if you don't have one.\n");
-                        exit(EXIT_FAILURE);
-                    }
-
-                    while(pinValid) {
-                        printf("\nATM System Menu\n===============\n\n");
-                        printf("1. Check Balance\n2. Deposit\n3. Withdraw\n4. View transaction history\n5. Quit\n\n");
-
-                        printf("Enter choice: ");
-                        scanf("%d", &choice2);
-
-                        switch(choice2) {
-                            case 1: showBalance(&balance);
-                                    break;
-                            case 2: depositMoney(&head, &balance);
-                                    break;
-                            case 3: withdrawMoney(&head, &balance);
-                                    break;
-                            case 4: showHistory(&head);
-                                    break;
-                            case 5: printf("\nThank you for using the ATM\n");
-                                    exit(EXIT_SUCCESS);
-                            default: printf("\nInvalid option entered!\n");
-                                     break;
-                        }
-                    }
-
-            case 3: exit(EXIT_SUCCESS);
-
-            default: printf("\nInvalid choice...Try again...\n");
-                     break;
+#include<stdio.h>
+#include<stdlib.h>
+#include<stdbool.h>
+#include<math.h>
+void login();
+void mainMenu();
+void checkBalance(float balance);
+float moneyDeposit(float balance);
+float moneyWithdraw(float balance);
+void menuExit();
+void errorMessage();
+int main() {
+    int option;
+    float balance = 15000.00;
+    int choose;
+    bool again = true;
+    while (again) {
+    mainMenu();
+    printf("=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-\n");
+    printf("Your Selection:\t");
+    scanf("%d", &option);
+        switch (option) {
+            case 1:
+             system("CLS");
+                checkBalance(balance);
+                break;
+            case 2:
+             system("CLS");
+                balance = moneyDeposit(balance);
+                break;
+            case 3:
+             system("CLS");
+                balance = moneyWithdraw(balance);
+                break;
+            case 4:
+             system("CLS");
+                menuExit();
+                return 0;
+            default:
+                errorMessage();
+                break;
         }
-    }
-    return 0;
-}
-
-
-int checkPin(void) {
-    FILE *fp;
-    char pin[8];
-    char keyPin[8];
-    int pinValid = 0;
-
-    printf("\n\nEnter the PIN: \n");
-    scanf("%s", keyPin);
-
-    fp = fopen("pin.txt", "r");
-    if (NULL == fp) {
-        printf("\nFile cannot be opened\n");
-        exit(EXIT_FAILURE);
-    }
-
-    while (fgets(pin, sizeof(pin), fp) != NULL) {
-        if (strstr(pin, keyPin)) {
-            pinValid = 1;
+        printf("=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=\n");
+        printf("Would you like to do another transaction:\n");
+        printf("< 1 > Yes\n");
+        printf("< 2 > No\n");
+        scanf("%d", &choose);
+        system("CLS");
+        if (choose == 2) {
+            again = false;
+            menuExit();
         }
+}
+     return 0;
+}
+void mainMenu() {
+    printf("*******Hello!******\n");
+    printf("***Welcome to ATM Banking****\n\n");
+    printf("*Please choose one of the options below*\n\n");
+    printf("< 1 >  Check Balance\n");
+    printf("< 2 >  Deposit\n");
+    printf("< 3 >  Withdraw\n");
+    printf("< 4 >  Exit\n\n");
+}
+void checkBalance(float balance) {
+    printf("You Choose to See your Balance\n");
+    printf("\n\n****Your Available Balance is:   $%.2f\n\n", balance);
+}
+float moneyDeposit(float balance) {
+    float deposit;
+    printf("You choose to Deposit a money\n");
+    printf("$$$$Your Balance is: $%.2f\n\n", balance);
+    printf("**Enter your amount to Deposit\n");
+    scanf("%f", &deposit);
+    balance += deposit;
+    printf("\n****Your New Balance is:   $%.2f\n\n", balance);
+    return balance;
+}
+float moneyWithdraw(float balance) {
+    float withdraw;
+    bool back = true;
+    printf("You choose to Withdraw a money\n");
+    printf("$$$$Your Balance is: $%.2f\n\n", balance);
+    while (back) {
+    printf("Enter your amount to withdraw:\n");
+    scanf("%f", &withdraw);
+    if (withdraw <balance) {
+        back = false;
+        balance -= withdraw;
+        printf("\n$$$$Your withdrawing money is:  $%.2f\n", withdraw);
+        printf("**Your New Balance is:   $%.2f\n\n", balance);
     }
-    fclose(fp);
-
-    return pinValid;
-}
-
-
-void pinGeneration(void) {
-    FILE *fp;
-    srand(time(NULL));
-    int generatedPin = 1000+rand()%9000;
-
-    printf("\nPIN generated successfully\n");
-    printf("\nYour generated PIN: %d\n", generatedPin);
-    printf("\nRe-run the program and use ATM with this PIN\n\n");
-
-    fp = fopen("pin.txt", "a");
-    if (NULL == fp) {
-        printf("\nCannot open file!");
-        exit(EXIT_FAILURE);
+        else  {
+        printf("You don't have enough money\n");
+        printf("Please contact to your Bank Customer Services\n");
+        printf("**Your Balance is:   $%.2f\n\n", balance);
     }
-
-    
-    fprintf(fp, "%d\n", generatedPin);
-    fclose(fp);
-}
-
-
-void showBalance(int *balance) {
-    printf("\nYour current balance is Rs.%d\n", *balance);
-}
-
-
-void depositMoney(node **head, int *balance) {
-    int depositAmount;
-    char depositStmt[50];
-
-    printf("\nEnter amount to deposit: ");
-    scanf("%d", &depositAmount);
-
-    if (depositAmount > 0) {
-        *balance += depositAmount;
-        printf("\nRs.%d deposited\n", depositAmount);
-
-        snprintf(depositStmt, sizeof(depositStmt), "Rs.%d deposited\n", depositAmount);
-        saveHistory(head, depositStmt);
-    } else {
-        printf("\nInvalid amount entered\n.");
     }
+    return balance;
 }
-
-void withdrawMoney(node **head, int *balance) {
-    int withdrawAmount;
-    char withdrawStmt[50];
-
-    printf("\nEnter amount to withdraw: ");
-    scanf("%d", &withdrawAmount);
-
-    if (withdrawAmount > 0) {
-        if (withdrawAmount > *balance) {
-            printf("\nCannot withdraw. Balance Rs.%d\n", *balance);
-        } else {
-            *balance -= withdrawAmount;
-            printf("\nRs.%d withdrawn\n", withdrawAmount);
-            snprintf(withdrawStmt, sizeof(withdrawStmt), "Rs.%d withdrawn\n", withdrawAmount);
-            saveHistory(head, withdrawStmt);
-        }
-    } else {
-        printf("\nInvalid amount entered\n.");
-    }
+void menuExit() {
+    printf("--------------Take your receipt!!!------------------\n");
+    printf("-----Thank you for using ATM Banking Machine!!!-----\n");
 }
-
-
-void saveHistory(node **head, char *str) {
-    static int count = 0;
-    node *temp;
-    temp = (node *)malloc(sizeof(node));
-
-    strcpy(temp->statement, str);
-    temp->link = NULL;
-
-    if (NULL == *head) {
-        *head = temp;
-        count++;
-    } else {
-        if (10 == count) {
-            removeHistory(head);
-            count--;
-        }
-        node *p;
-        p = *head;
-        while (NULL != p->link) {
-            p = p->link;
-        }
-        p->link = temp;
-        count++;
-    }
-}
-
-void removeHistory(node **head) {
-    node *temp;
-    temp = *head;
-    *head = (*head)->link;
-    temp->link = NULL;
-    free(temp);
-}
-
-
-void showHistory(node **head) {
-    node *temp;
-    temp = *head;
-
-    if (NULL == temp) {
-        printf("\nNo transaction history...\n");
-    } else {
-        printf("\nTransaction History\n-------------------\n\n");
-        while (NULL != temp) {
-            printf("%s\n", temp->statement);
-            temp = temp->link;
-        }
-    }
+ 
+void errorMessage() {;
+    printf("****!!!You selected invalid number!!!****\n");
 }
